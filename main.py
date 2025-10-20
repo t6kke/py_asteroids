1f3497174efcb07a90557d3060a63147bd4d7e19
Martin <m.t6kke@gmail.com>
2025-10-20 18:17:16 +0300

    lessons 1-5 done, player object is being dawn in the game window

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import pygame
 
-def main():
-    print("Starting Asteroids!")
+from constants import *
+from player import Player
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60)/1000 #fps limiter
 
 if __name__ == "__main__":
     main()
